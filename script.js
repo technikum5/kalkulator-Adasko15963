b9f166e49f6123ea19c85ca09ccca24d7618a264
Adasko15963 <adam.gajewski159@gmail.com>
2024-11-29 17:26:24 +0100

    Adam G 5 4t

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 function calculate(operation) {
-//Dpkończ kod - pobierz liczby i sprawdź czy są poprawne
+    let n1 = parseFloat(document.getElementById('n1').value);
+    let n2 = parseFloat(document.getElementById('n2').value);
+    if (isNaN(n1) && isNaN(n2)){
         alert('Proszę wprowadzić prawidłowe liczby.');
         return;
     }
-
     switch (operation) {
-       //Dokończ funkcję działań: dodawania, odejmowania, dzielenia, mnożenia
-       
-             //uzupełnij   alert('Nie można dzielić przez zero.');
-            alert('Nieznana operacja.');
-            return;
-    }
-
+        case "dod":
+            result = n1+n2;
+            
+            break;
+        case "ode":
+            result = n1 -n2;
+            break;
+        case "pom":
+            result = n1 * n2;
+            break;
+        case "pod":
+            if(n2 == 0){
+                alert('Nie można dzielić przez 0.');
+                break;
+            }else{
+                result = n1 / n2;
+                break;
+            }
+        }
     document.getElementById('result').textContent = result;
 }
